ba77a8a953160e61b5f233abf2c476bf5f45ee39
Utkarsh Gayguwal <utkarshgayguwal123@gmail.com>
2025-05-01 08:36:30 +0000

    Added Response trait to shorten json response(in controller).

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,50 +16,27 @@ class UserController extends Controller
         try{
             $roleId = Role::where("name", $request->role)->value("id");
             $user->roles()->attach($roleId);
-            return response()->json([
-                "message" => "Role assigned successfully.",
-                "status" => 201
-            ]);
+            return $this->successResponse(message: "Role assigned successfully.");
         }catch(Exception $e){
-            return response()->json([
-                "message" => "Server Error.",
-                "status" => 500
-            ]);
+            return $this->errorResponse();
         }
     }
 
     public function getUserRole($id){
         try{
             $user = User::find($id)->with("roles")->first();
-            return response()->json([
-                "message" => "User roles retrieved successfully.",
-                "status" => 200,
-                "data" => new UserRoleResource($user)
-            ]);
-
+            return $this->successResponse(data: new UserRoleResource($user));
         }catch(Exception $e){
-            throw $e;
-            return response()->json([
-                "message" => "Server Error.",
-                "status" => 500
-            ]);
+            return $this->errorResponse();
         }
     }
 
     public function getRoles(){
         try{
             $roles = Role::select('id', 'name', 'slug')->get();
-            return response()->json([
-                "message" => "Roles retrieved successfully.",
-                "status" => 200,
-                "data" => RoleResource::collection($roles)
-            ]);
+            return $this->successResponse(data: RoleResource::collection($roles));
         }catch(Exception $e){
-            throw $e;
-            return response()->json([
-                "message" => "Server Error.",
-                "status" => 500
-            ]);
+            return $this->errorResponse();
         }
     }
 
--- app/Traits/Response.php
+++ app/Traits/Response.php
@@ -1,8 +1,72 @@
 <?php
 
-namespace App;
+namespace App\Traits;
 
 trait Response
 {
-    //
+    const SUCCESS = 200;
+    const CREATED = 201;
+    const NO_CONTENT = 204;
+    const BAD_REQUEST = 400;
+    const UNAUTHORIZED = 401;
+    const FORBIDDEN = 403;
+    const NOT_FOUND = 404;
+    const METHOD_NOT_ALLOWED = 405;
+    const INTERNAL_SERVER_ERROR = 500;
+    const SERVICE_UNAVAILABLE = 503;
+
+    public function badRequestResponse($message = "Bad Request.", $status = self::BAD_REQUEST)
+    {
+        return response()->json([
+            "message" => $message,
+            "status" => $status
+        ], $status);
+    }
+
+    public function unauthorizedResponse($message = "Unauthorized.", $status = self::UNAUTHORIZED)
+    {
+        return response()->json([
+            "message" => $message,
+            "status" => $status
+        ], $status);
+    }
+
+    public function forbiddenResponse($message = "Forbidden.", $status = self::FORBIDDEN)
+    {
+        return response()->json([
+            "message" => $message,
+            "status" => $status
+        ], $status);
+    }
+
+    public function notFoundResponse($message = "Resource not found.", $status = self::NOT_FOUND)
+    {
+        return response()->json([
+            "message" => $message,
+            "status" => $status
+        ], $status);
+    }
+    public function successResponse($data = null, $message = "Resource fetched successfully.", $status = self::SUCCESS)  
+    {
+        if($status == self::CREATED){
+            return response()->json([
+                "message" => $message,
+                "status" => $status,
+            ], $status);
+        }else{
+            return response()->json([
+                "message" => $message,
+                "status" => $status,
+                "data" => $data
+            ], $status);
+        }
+    }
+
+    public function errorResponse($message = "Server Error.", $status = self::INTERNAL_SERVER_ERROR)
+    {
+        return response()->json([
+            "message" => $message,
+            "status" => $status
+        ], $status);
+    }
 }
